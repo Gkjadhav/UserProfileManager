68f7487e5d38334353a5871466e37d77752de00e
Gkjadhav <92724411+Gkjadhav@users.noreply.github.com>
2026-08-28 22:42:28 +0530

    Add user model

---
+++ src/UserProfileManager/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace UserProfileManager.Models
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string FullName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? UserInfo { get; set; }
+        public string? LinkedInProfile { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
